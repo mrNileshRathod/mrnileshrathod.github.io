abbbe504f2ac4ce169f5c7223ea08c5ceb5f97a1
Nilesh Rathod <nr1157161@gmail.com>
2021-07-25 15:57:53 -1200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,24 +11,6 @@ var typed = new Typed('.animate', {
     loop: true,
 });
 
-/*==================== Scroll Effect ====================*/
-// let navbarlinks = select('#navbar .scrollto', true)
-// const navbarlinksActive = () => {
-//     let position = window.scrollY + 200
-//     navbarlinks.forEach(navbarlink => {
-//         if (!navbarlink.hash) return
-//         let section = select(navbarlink.hash)
-//         if (!section) return
-//         if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-//             navbarlink.classList.add('active')
-//         } else {
-//             navbarlink.classList.remove('active')
-//         }
-//     })
-// }
-// window.addEventListener('load', navbarlinksActive)
-// onscroll(document, navbarlinksActive)
-
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
@@ -106,6 +88,10 @@ tabs.forEach(tab => {
 var swiper = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
